a059f4102c30f0bb62617791d545581d0b39ead7
BikashGosai <bikashgosai@hotmail.com>
2019-01-14 15:09:27 +0545

    Second commit- Beta

--- CopyToClipboard/Form1.Designer.cs
+++ CopyToClipboard/Form1.Designer.cs
@@ -42,12 +42,13 @@ namespace CopyToClipboard
             this.Text5 = new System.Windows.Forms.Label();
             this.Text6 = new System.Windows.Forms.Label();
             this.btnPaste = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnCopy1
             // 
             this.btnCopy1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCopy1.Location = new System.Drawing.Point(100, 69);
+            this.btnCopy1.Location = new System.Drawing.Point(22, 64);
             this.btnCopy1.Margin = new System.Windows.Forms.Padding(2);
             this.btnCopy1.Name = "btnCopy1";
             this.btnCopy1.Size = new System.Drawing.Size(97, 28);
@@ -59,18 +60,19 @@ namespace CopyToClipboard
             // title
             // 
             this.title.AutoSize = true;
-            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.title.Location = new System.Drawing.Point(187, 21);
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.title.Location = new System.Drawing.Point(224, 20);
             this.title.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.title.Name = "title";
-            this.title.Size = new System.Drawing.Size(195, 26);
-            this.title.TabIndex = 3;
+            this.title.Size = new System.Drawing.Size(212, 26);
+            this.title.TabIndex = 10;
             this.title.Text = "Copy To ClipBoard";
             // 
             // btnCopy2
             // 
             this.btnCopy2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCopy2.Location = new System.Drawing.Point(100, 101);
+            this.btnCopy2.Location = new System.Drawing.Point(22, 96);
             this.btnCopy2.Margin = new System.Windows.Forms.Padding(2);
             this.btnCopy2.Name = "btnCopy2";
             this.btnCopy2.Size = new System.Drawing.Size(97, 28);
@@ -82,7 +84,7 @@ namespace CopyToClipboard
             // btnCopy6
             // 
             this.btnCopy6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCopy6.Location = new System.Drawing.Point(100, 229);
+            this.btnCopy6.Location = new System.Drawing.Point(22, 224);
             this.btnCopy6.Margin = new System.Windows.Forms.Padding(2);
             this.btnCopy6.Name = "btnCopy6";
             this.btnCopy6.Size = new System.Drawing.Size(97, 28);
@@ -94,7 +96,7 @@ namespace CopyToClipboard
             // btnCopy5
             // 
             this.btnCopy5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCopy5.Location = new System.Drawing.Point(100, 197);
+            this.btnCopy5.Location = new System.Drawing.Point(22, 192);
             this.btnCopy5.Margin = new System.Windows.Forms.Padding(2);
             this.btnCopy5.Name = "btnCopy5";
             this.btnCopy5.Size = new System.Drawing.Size(97, 28);
@@ -106,7 +108,7 @@ namespace CopyToClipboard
             // btnCopy4
             // 
             this.btnCopy4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCopy4.Location = new System.Drawing.Point(100, 165);
+            this.btnCopy4.Location = new System.Drawing.Point(22, 160);
             this.btnCopy4.Margin = new System.Windows.Forms.Padding(2);
             this.btnCopy4.Name = "btnCopy4";
             this.btnCopy4.Size = new System.Drawing.Size(97, 28);
@@ -118,7 +120,7 @@ namespace CopyToClipboard
             // btnCopy3
             // 
             this.btnCopy3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCopy3.Location = new System.Drawing.Point(100, 133);
+            this.btnCopy3.Location = new System.Drawing.Point(22, 128);
             this.btnCopy3.Margin = new System.Windows.Forms.Padding(2);
             this.btnCopy3.Name = "btnCopy3";
             this.btnCopy3.Size = new System.Drawing.Size(97, 28);
@@ -134,10 +136,10 @@ namespace CopyToClipboard
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Text1.AutoSize = true;
             this.Text1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Text1.Location = new System.Drawing.Point(232, 72);
+            this.Text1.Location = new System.Drawing.Point(151, 68);
             this.Text1.Name = "Text1";
             this.Text1.Size = new System.Drawing.Size(52, 20);
-            this.Text1.TabIndex = 9;
+            this.Text1.TabIndex = 1;
             this.Text1.Text = "Text 1";
             // 
             // Text2
@@ -147,10 +149,10 @@ namespace CopyToClipboard
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Text2.AutoSize = true;
             this.Text2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Text2.Location = new System.Drawing.Point(232, 104);
+            this.Text2.Location = new System.Drawing.Point(151, 100);
             this.Text2.Name = "Text2";
             this.Text2.Size = new System.Drawing.Size(52, 20);
-            this.Text2.TabIndex = 10;
+            this.Text2.TabIndex = 2;
             this.Text2.Text = "Text 2";
             // 
             // Text3
@@ -160,10 +162,10 @@ namespace CopyToClipboard
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Text3.AutoSize = true;
             this.Text3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Text3.Location = new System.Drawing.Point(232, 136);
+            this.Text3.Location = new System.Drawing.Point(151, 132);
             this.Text3.Name = "Text3";
             this.Text3.Size = new System.Drawing.Size(52, 20);
-            this.Text3.TabIndex = 11;
+            this.Text3.TabIndex = 3;
             this.Text3.Text = "Text 3";
             // 
             // Text4
@@ -173,10 +175,10 @@ namespace CopyToClipboard
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Text4.AutoSize = true;
             this.Text4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Text4.Location = new System.Drawing.Point(232, 168);
+            this.Text4.Location = new System.Drawing.Point(151, 164);
             this.Text4.Name = "Text4";
             this.Text4.Size = new System.Drawing.Size(52, 20);
-            this.Text4.TabIndex = 12;
+            this.Text4.TabIndex = 4;
             this.Text4.Text = "Text 4";
             // 
             // Text5
@@ -186,10 +188,10 @@ namespace CopyToClipboard
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Text5.AutoSize = true;
             this.Text5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Text5.Location = new System.Drawing.Point(232, 200);
+            this.Text5.Location = new System.Drawing.Point(151, 196);
             this.Text5.Name = "Text5";
             this.Text5.Size = new System.Drawing.Size(52, 20);
-            this.Text5.TabIndex = 13;
+            this.Text5.TabIndex = 5;
             this.Text5.Text = "Text 5";
             // 
             // Text6
@@ -199,19 +201,20 @@ namespace CopyToClipboard
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Text6.AutoSize = true;
             this.Text6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Text6.Location = new System.Drawing.Point(232, 232);
+            this.Text6.Location = new System.Drawing.Point(151, 228);
             this.Text6.Name = "Text6";
             this.Text6.Size = new System.Drawing.Size(52, 20);
-            this.Text6.TabIndex = 14;
+            this.Text6.TabIndex = 6;
             this.Text6.Text = "Text 6";
             // 
             // btnPaste
             // 
+            this.btnPaste.Cursor = System.Windows.Forms.Cursors.WaitCursor;
             this.btnPaste.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPaste.Location = new System.Drawing.Point(332, 85);
+            this.btnPaste.Location = new System.Drawing.Point(507, 84);
             this.btnPaste.Margin = new System.Windows.Forms.Padding(2);
             this.btnPaste.Name = "btnPaste";
-            this.btnPaste.Size = new System.Drawing.Size(150, 160);
+            this.btnPaste.Size = new System.Drawing.Size(150, 152);
             this.btnPaste.TabIndex = 15;
             this.btnPaste.Text = "Click To Paste";
             this.btnPaste.UseCompatibleTextRendering = true;
@@ -219,11 +222,22 @@ namespace CopyToClipboard
             this.btnPaste.UseWaitCursor = true;
             this.btnPaste.Click += new System.EventHandler(this.btnPaste_Click);
             // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(507, 46);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(150, 23);
+            this.btnClose.TabIndex = 16;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.ClientSize = new System.Drawing.Size(692, 292);
+            this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnPaste);
             this.Controls.Add(this.Text6);
             this.Controls.Add(this.Text5);
@@ -241,6 +255,7 @@ namespace CopyToClipboard
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -262,6 +277,7 @@ namespace CopyToClipboard
         private System.Windows.Forms.Label Text5;
         private System.Windows.Forms.Label Text6;
         private System.Windows.Forms.Button btnPaste;
+        private System.Windows.Forms.Button btnClose;
     }
 }
 
--- CopyToClipboard/Form1.cs
+++ CopyToClipboard/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,11 +18,41 @@ namespace CopyToClipboard
     {
         //string[] arrayStrings = new string[10];
         public static List<string> arrayStrings = new List<string>();
+        string[] format = new string[] { "MM/dd/yyyy HH:mm:ss" };
         public Form1()
         {
             InitializeComponent();
+            ReadFile();
         }
+        private void ReadFile()
+        {
+            string path = "C:\\Users\\Bikash1\\Desktop\\CTC\\SavedList.txt";
+            if (Directory.Exists("C:\\Users\\Bikash1\\Desktop\\CTC"))
+            {
+                if (File.Exists(path))
+                {
+                    int concedeDateTime = 0;
+                    using (StreamReader SRead = new StreamReader(path))
+                    {
+                        // Use while not null pattern in while loop.
+                        string line;
+                        while ((line = SRead.ReadLine()) != null)
+                        {
+                            DateTime datetime;
 
+                            if (DateTime.TryParseExact(line, format, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.NoCurrentDateDefault, out datetime))
+                                concedeDateTime++;
+                            else if (concedeDateTime > 1)
+                                break;
+                            else if (line != null && line.Trim() != string.Empty)
+                                arrayStrings.Add(line.Trim());
+                        }
+                    }
+                    setValues(arrayStrings);
+                }
+            }
+
+        }
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
 
@@ -65,21 +96,26 @@ namespace CopyToClipboard
 
         public void showMessage(string msg)
         {
-            MessageBox.Show(msg);
+            MessageBox.Show(msg, "Information", MessageBoxButtons.OK);
         }
         public void setValues(List<string> list)
         {
-            int count = 0;
-            count = list.Count();
+            int listCount = 0;
+            int valuesSetCount = 0;
+            listCount = list.Count();
             List<Label> listCtrl = new List<Label>();
             listCtrl = Controls.OfType<Label>().ToList();
-            for (int i = 0; i < count; i++)
+            for (int label = 0; label < listCtrl.Count; label++)
             {
-                for (int label = 1; label < listCtrl.Count; label++)
+                for (int i = 0; i < listCount; i++)
                 {
-                    if (i + 2 == label + 1)
+                    if (i + 1 == listCtrl[label].TabIndex && listCtrl[label].Name != "title")
                     {
                         listCtrl[label].Text = list[i];
+                        valuesSetCount++;
+
+                        if (valuesSetCount == listCount)
+                            break;
                     }
                 }
             }
@@ -92,24 +128,86 @@ namespace CopyToClipboard
             if (iData.GetDataPresent(DataFormats.Text))
             {
                 Text1.Text = (string)iData.GetData(DataFormats.Text);
-                if (arrayStrings.Count() == 0)
-                {
-                    arrayStrings.Add(Text1.Text);
-                }
 
                 if (arrayStrings.Count() > 0 && !arrayStrings.Contains(Text1.Text))
                 {
-                    arrayStrings.Add(Text1.Text);
+                    //arrayStrings = Rearrange(arrayStrings);
+                    arrayStrings.Insert(0, Text1.Text);
                     setValues(arrayStrings);
                 }
-
-
+                //if the string/item is in the list of string
+                else if (arrayStrings.Contains(Text1.Text))
+                {
+                    int index = arrayStrings.FindIndex(x => x.StartsWith(Text1.Text));
+                    arrayStrings.RemoveAt(index);
+                    //arrayStrings = Rearrange(arrayStrings);
+                    arrayStrings.Insert(0, Text1.Text);
+                    setValues(arrayStrings);
+                }
+                else
+                {
+                    arrayStrings.Insert(0, Text1.Text);
+                }
             }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            Form1 a = new Form1();
+        }
 
+        public void writetoFile(List<string> list)
+        {
+            using (TextWriter tw = new StreamWriter("C:\\Users\\Bikash1\\Desktop\\CTC\\SavedList.txt"))
+            {
+                tw.WriteLine(DateTime.Now.ToString(format[0]));
+                tw.WriteLine("--------------------------------------");
+                foreach (String s in list)
+                {
+                    tw.WriteLine(s);
+                }
+                tw.WriteLine("--------------------------------------");
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            // Display a MsgBox asking the user to save changes or abort.
+            if (MessageBox.Show("Do you want to save changes?", "CTC", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                // Cancel the Closing event from closing the form.
+
+                writetoFile(arrayStrings);
+                Application.Exit();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Display a MsgBox asking the user to save changes or abort.
+            if (MessageBox.Show("Do you want to save changes?", "CTC", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                //Save the changes.                
+                writetoFile(arrayStrings);
+                
+            }
+        }
+        private List<string> Rearrange(List<string> arrayStrings)
+        {
+            List<string> temp = new List<string>();
+            int count = arrayStrings.Count;
+            temp = arrayStrings;
+
+            for (int i = 0; i < count; i++)
+            {
+                string tempString = string.Empty;
+                if (arrayStrings[i] != string.Empty)
+                    temp.Insert(i + 1, arrayStrings[i]);
+
+                //do nothing
+
+            }
+            return temp;
         }
     }
 }
